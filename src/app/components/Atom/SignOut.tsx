ad0dc0044f52f1f37c4c9c270b207ee5e6f52fff
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 18:18:27 +0900

    Apply CSS to SignOut component

--- src/app/components/Atom/SignOut.tsx
+++ src/app/components/Atom/SignOut.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@mui/material';
 import { auth } from '@/app/data/Firebase';
 import React from 'react';
+import CallIcon from '@mui/icons-material/Call';
 
 const SignOut = () => {
   return (
     <div>
       <Button onClick={() => auth.signOut()}>Sign Out</Button>
+      <h3>{auth.currentUser?.displayName}</h3>
+      <CallIcon />
     </div>
   );
 };
--- src/app/components/Molecules/SendMessage.tsx
+++ src/app/components/Molecules/SendMessage.tsx
@@ -1,8 +1,26 @@
 'use client';
 
+import css from 'styled-jsx/css';
 import React, { useState } from 'react';
 import { auth, db } from '@/app/data/Firebase';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { Input } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
+const styles = css`
+  .message > :global(.message-input) {
+    width: 80%;
+    font-size: 16px;
+    font-weight: 500;
+    margin-left: 4px;
+    margin: 0 0 4px 4px;
+  }
+
+  .message > :global(.message-icon) {
+    color: #7ac2ff;
+    margin-left: 20px;
+  }
+`;
 
 const SendMessage = () => {
   const [message, setMessage] = useState('');
@@ -26,14 +44,17 @@ const SendMessage = () => {
 
   return (
     <>
+      <style jsx>{styles}</style>
       <form onSubmit={sendMessage}>
-        <div>
-          <input
+        <div className='message'>
+          <Input
             type='text'
             value={message}
             placeholder='Put down your message'
+            className='message-input'
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
           />
+          <SendIcon className='message-icon' />
         </div>
       </form>
     </>

6d9dc84e8dbc7f8ff64260bc0473d8a26194b2d2
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 16:31:37 +0900

    Create components

--- src/app/components/Atom/SignIn.tsx
+++ src/app/components/Atom/SignIn.tsx
@@ -1,21 +1,14 @@
 'use client';
 
-import { auth } from '@/app/data/Firebase';
+import { auth, provider } from '@/app/data/Firebase';
 import { Button } from '@mui/material';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { signInWithPopup } from 'firebase/auth';
 
 const SignIn = () => {
   const signInWithGoogle = () => {
-    // Googleの認証機能を使用可能にする
-    const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-
-        // The signed-in user info.
-        const user = result.user;
+        console.log('success');
       })
       .catch((error) => {
         console.error(error);
@@ -30,6 +23,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-// 1. FIrebaseの初期化
-// 2. Firestoreの初期化・値を渡す
---
+++ src/app/components/Organisms/Line.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignOut from '../Atom/Signout';
+
+const Line = () => {
+  return (
+    <div>
+    <SignOut />
+  </div>
+)
+};
+
+export default Line;

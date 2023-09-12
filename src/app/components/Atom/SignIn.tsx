da6b9eecf90d0514014cbfbc29b1dce230247433
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 15:52:52 +0900

    Initialize Firebase and enable google authentification

--- src/app/components/Atom/SignIn.tsx
+++ src/app/components/Atom/SignIn.tsx
@@ -1,18 +1,35 @@
-import { Button } from '@mui/material'
+'use client';
+
+import { auth } from '@/app/data/Firebase';
+import { Button } from '@mui/material';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 
 const SignIn = () => {
   const signInWithGoogle = () => {
-    // fireBaseのDBにデータ追加
-  }
+    // Googleの認証機能を使用可能にする
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
       <Button onClick={signInWithGoogle}>Login With Google</Button>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
 
 // 1. FIrebaseの初期化
 // 2. Firestoreの初期化・値を渡す
---
+++ src/app/data/Firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDaTYhVKJU5_t90U3EqIjMWHxYwsyph-VU',
+  authDomain: 'line-app-93f7f.firebaseapp.com',
+  projectId: 'line-app-93f7f',
+  storageBucket: 'line-app-93f7f.appspot.com',
+  messagingSenderId: '800360565946',
+  appId: '1:800360565946:web:b6873774bf51784d610683',
+};
+
+// 1. Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// 2. Initialize Firestore(DB) passing app to getFireStore()
+const db = getFirestore(app);
+// 3. Get Authentification info with firebase.auth()
+const auth = getAuth();
+
+export { db, auth };

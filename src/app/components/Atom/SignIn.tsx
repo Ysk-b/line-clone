7597500ba6d0fe2627028088101474cff4689487
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 15:21:35 +0900

    Install Firebase

--- src/app/components/Atom/SignIn.tsx
+++ src/app/components/Atom/SignIn.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Button } from '@mui/material'
 
 const SignIn = () => {
+  const signInWithGoogle = () => {
+    // fireBaseのDBにデータ追加
+  }
+
   return (
-    <div>SignIn</div>
+    <div>
+      <Button onClick={signInWithGoogle}>Login With Google</Button>
+    </div>
   )
 }
 
 export default SignIn
+
+// 1. FIrebaseの初期化
+// 2. Firestoreの初期化・値を渡す
